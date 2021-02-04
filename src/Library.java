56d68f71fff8e5f70233abe4409beb773d1dd13f
Wojager <ireneusz.dakowski@gmail.com>
2021-02-04 09:30:35 +0100

    library-005 constructor overloading

--- src/Library.java
+++ src/Library.java
@@ -1,10 +1,10 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.4";
 
         Book book1 = new Book("w pustyni i w puszczy", "Henryk Sienkiewicz", 1996, 297, "Greg", "1234567890123" );
         Book book2 = new Book("Diuna II", "John Doe", 1988, 199, "pwn", "9879879879" );
-        Book book3 = new Book("John Wick I", "John Wick", 2018, 456, "MSI", "7657657657" );
+        Book book3 = new Book("John Wick I", "John Wick", 2018, 456, "MSI");
 
 
         System.out.println(appName);
